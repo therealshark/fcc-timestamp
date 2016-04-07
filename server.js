64d69366c5785bb335596aaf57a6a46f2c14dbeb
theshark <mirco.weigel@web.de>
2016-04-07 15:02:55 +0000

    implemented timestamp

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const http = require('http');
+'use strict';
 
-http.createServer((req, res) => {
-    res.end('Hello World');
-}).listen(process.env.PORT || 5000);
+const express = require('express'),
+    moment = require('moment');
+    
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/:date', (req, res)  => {
+    const param = req.params.date;
+    let date;
+    
+    if(param == Number(param)){ // unix timestamp
+        date = moment(param, 'x');
+    }else{ // natural date
+        date = moment(param, 'MMMM DD, YYYY');
+    }
+    
+    res.json({
+        unix: date.format('x'),
+        natural: date.format('MMMM DD, YYYY')
+    });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
